4ea323d17d0ed8b5319f5652e31324dcebb53d0b
Maria Kirsanova <kopf@list.ru>
2024-10-19 01:55:31 +0300

    Ver. 1.13 fix user auth & redoc

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
@@ -15,5 +16,7 @@ router.register('users', CustomUserViewSet, 'users')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include('djoser.urls')),
+    path('docs/', TemplateView.as_view(template_name='docs/redoc.html'),
+         name='redoc'),
     path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from users.models import Follow
+from users.models import Follow, User
 
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = ('email', 'first_name')
+    empty_value_display = '-empty-'
 
 @admin.register(Follow)
 class SubscriptionAdmin(admin.ModelAdmin):
